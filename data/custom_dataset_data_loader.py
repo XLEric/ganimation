8b8fe7ed4f93222a51400ee90e1f5ecdadf58a17
eric <koke8756@qq.com>
2020-09-29 01:49:09 +0800

    update

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -17,7 +17,6 @@ class CustomDatasetDataLoader:
             batch_size=self._opt.batch_size,
             shuffle=True,
             num_workers=int(self._num_threds),
-            num_workers = 4,
             drop_last=True)
 
     def load_data(self):
